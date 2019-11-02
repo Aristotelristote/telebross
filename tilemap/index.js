1df382593f03468121aa8f4e7e40b21dfa7cfaed
aristóteles <54686090+Aristotelristote@users.noreply.github.com>
2019-11-01 22:59:18 -0300

    teste telimap
    versão de teste do tilemap

---
+++ tilemap/index.js
@@ -0,0 +1,30 @@
+import { start } from "./start.js";
+import { fase1 } from "./fase1.js";
+import { fase2 } from "./fase2.js";
+import { gameover1 } from "./gameover1.js";
+import { gameover2 } from "./gameover2.js";
+
+var config = {
+  type: Phaser.AUTO,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 390
+      },
+      debug: true
+    }
+  },
+  // Suporte a tela cheia
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: "game",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
+  // Várias cenas, em sequência
+  scene: [fase1, gameover1, fase2, gameover2] //removendo gameover2 funciona
+};
+
+var game = new Phaser.Game(config);
