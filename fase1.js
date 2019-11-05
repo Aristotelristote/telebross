a7392989b740b7a9557faa11a69ed159b5ffbd79
Ederson Torresini <boidacarapreta@gmail.com>
2019-11-05 01:47:23 +0000

    Testar tilemap original

---
+++ fase1.js
@@ -0,0 +1,186 @@
+import { gameover1 } from "./gameover1.js";
+import { fase2 } from "./fase2.js";
+
+//criação do player 1
+var player;
+
+//movimtação de câmeras
+var moveCam = false;
+
+//movimentação personagens
+var cursors;
+
+// Variáveis do tilemap
+var map;
+var chão;
+var parede;
+var camada1;
+
+var fase1 = new Phaser.Scene("fase1");
+
+fase1.preload = function() {
+  //carregando imagens em geral
+  this.load.image("parede", "assets/parede.png");
+  this.load.image("plataforma", "assets/plataforma.png");
+  this.load.image("bloco", "assets/bloco.png");
+  this.load.image("blocolongo", "assets/bloco2.png");
+  this.load.image("star", "assets/dude.png");
+  this.load.image("telefone", "assets/fases/fase1/telefone.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.image("boi", "assets/boi.png");
+  this.load.image("porta", "assets/portaverde.png");
+
+  //animações dos personagem
+  this.load.spritesheet("idle", "assets/ifiano/idle.png", {
+    frameWidth: 38,
+    frameHeight: 62
+  });
+  this.load.spritesheet("run", "assets/ifiano/run.png", {
+    frameWidth: 43,
+    frameHeight: 62
+  });
+  this.load.spritesheet("runleft", "assets/ifiano/runleft.png", {
+    frameWidth: 43,
+    frameHeight: 62
+  });
+  this.load.spritesheet("dead", "assets/ifiano/dead.png", {
+    frameWidth: 77,
+    frameHeight: 62
+  });
+  //mapa jogo
+  this.load.tilemapTiledJSON("fase1", "assets/fases/fase1/fase1.json");
+};
+
+fase1.create = function() {
+  /*
+  var map = this.add.tilemap("fase1");
+  var terrain = map.addTilesetImage("cavernaSemfundo", "caverna");
+  var terrain2 = map.addTilesetImage("superficie", "superficie");
+  var terrain3 = map.addTilesetImage("background", "background");
+
+  //criando os niveis do mapa
+  var topLayer3 = map.createStaticLayer("topLayer3", [terrain3], 0, 0);
+  var topLayer = map.createStaticLayer("topLayer", [terrain], 0, 0);
+  var topLayer2 = map.createStaticLayer("topLayer2", [terrain2], 0, 0);
+
+  // ADICIONANDO COLISAO AO JOGO
+  this.physics.add.collider(player, topLayer);
+  this.physics.add.collider(player, topLayer2);
+
+  topLayer.setCollisionByProperty({ collides: true });
+  topLayer2.setCollisionByProperty({ collides: true });
+  */
+
+  //criando o mapa
+  map = this.add.tilemap("fase1");
+  chão = map.addTilesetImage("plataforma", "plataforma");
+  parede = map.addTilesetImage("parede", "parede");
+  camada1 = map.createStaticLayer("camada1", [chão, parede], 0, 0);
+
+  //parte de movimentação de cameras
+  this.cameras.main.setBounds(0, 0, 3200, 600);
+  this.physics.world.setBounds(0, 0, 3200, 600);
+
+  // The player and its settings
+  player = this.physics.add.sprite(100, 450, "idle");
+  //player2 = this.physics.add.sprite(150, 450, 'idle');
+  this.physics.add.collider(player, camada1);
+  camada1.setCollisionByProperty({ collides: true });
+
+  //parte do player com cameras
+  this.cameras.main.startFollow(player, true, 0.05, 0.05);
+  //this.cameras.main.startFollow(player2, true, 0.05, 0.05);
+
+  //  Player physics properties. Give the little guy a slight bounce.
+  player.setCollideWorldBounds(true);
+  //player2.setCollideWorldBounds(true);
+
+  //  Our player animations, turning, walking left and walking right.
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("runleft", {
+      start: 0,
+      end: 15
+    }),
+    frameRate: 15,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: this.anims.generateFrameNumbers("idle", {
+      start: 0,
+      end: 15
+    }),
+    frameRate: 20,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("run", {
+      start: 0,
+      end: 15
+    }),
+    frameRate: 20,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "dead",
+    frames: this.anims.generateFrameNumbers("dead", {
+      start: 0,
+      end: 16
+    }),
+    frameRate: 10,
+    repeat: 0
+  });
+
+  //  Input Events
+  cursors = this.input.keyboard.createCursorKeys();
+  //física do mapa
+  this.physics.add.collider(player, chão);
+  /*
+  chão.setCollisionByProperty({
+    collides: true
+  });
+  */
+};
+
+fase1.update = function() {
+  //teste parte de movimentação de icones na tela
+
+  var cam = this.cameras.main;
+
+  if (moveCam) {
+    if (cursors.left.isDown) {
+      cam.scrollX -= 4;
+    } else if (cursors.right.isDown) {
+      cam.scrollX += 4;
+    }
+  }
+
+  if (cursors.up.isDown) {
+    cam.scrollY -= 4;
+  } else if (cursors.down.isDown) {
+    cam.scrollY += 4;
+  }
+
+  //movimentação do personagem 1
+  else if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else if (cursors.up.isUp && cursors.left.isUp && cursors.right.isUp) {
+    player.setVelocityX(0);
+    player.anims.play("turn");
+  }
+  if (cursors.up.isDown && player.body.blocked.down) {
+    player.setVelocityY(-330);
+  }
+};
+
+export { fase1 };
---
+++ gameover1.js
@@ -0,0 +1,142 @@
+import { fase1 } from "./fase1.js";
+
+var player;
+var scoreText;
+
+var platforms;
+
+var gameOver = true;
+var music;
+
+var gameover1 = new Phaser.Scene("gameover1");
+
+gameover1.preload = function() {
+  this.load.image("parede", "assets/parede.png");
+  this.load.image("ground", "assets/plataforma.png");
+  this.load.image("bloco", "assets/bloco.png");
+  this.load.image("blocolongo", "assets/bloco2.png");
+  this.load.image("morto", "assets/ifiano/morto.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.image("boi", "assets/boi.png");
+  this.load.image("porta", "assets/saida.png");
+  this.load.image("reiniciar", "assets/reiniciar.png");
+
+  this.load.spritesheet("idle", "assets/ifiano/idle.png", {
+    frameWidth: 38,
+    frameHeight: 62
+  });
+  this.load.spritesheet("dead", "assets/ifiano/dead.png", {
+    frameWidth: 75,
+    frameHeight: 62
+  });
+  this.load.spritesheet("fullscreen", "assets/fullscreen.png", {
+    frameWidth: 64,
+    frameHeight: 64
+  });
+  this.load.audio("music", "assets/music.mp3");
+};
+
+gameover1.create = function() {
+  //  A simple background for our game
+  this.add.image(400, 300, "parede");
+  this.add.image(1200, 300, "parede");
+
+  this.add.image(500, 510, "porta");
+
+  //  The score
+  this.scoreText = this.add.text(16, 16, "score: 0", {
+    fontSize: "32px",
+    fill: "#000"
+  });
+  //texto GameOver
+  this.GameOverText = this.add.text(100, 100, "Tente Novamente", {
+    fontSize: "64px",
+    fill: "#000"
+  });
+  this.GameOverText = this.add.text(200, 200, "clique aqui", {
+    fontSize: "64px",
+    fill: "#000"
+  });
+  this.GameOverText.visible = true;
+
+  //  The platforms group contains the ground and the 2 ledges we can jump on
+  platforms = this.physics.add.staticGroup();
+
+  //  Here we create the ground.
+  //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  platforms
+    .create(400, 700, "ground")
+    .setScale(2)
+    .refreshBody(); // chão
+  platforms
+    .create(1200, 700, "ground")
+    .setScale(2)
+    .refreshBody(); //chão
+  platforms
+    .create(600, 440, "bloco")
+    .setScale(2)
+    .refreshBody(); //nivel 1
+  platforms
+    .create(350, 310, "blocolongo")
+    .setScale(2)
+    .refreshBody(); //nivel 2
+
+  // The player and its settings
+  player = this.physics.add.sprite(100, 450, "morto");
+
+  player.setCollideWorldBounds(true);
+
+  this.physics.add.collider(player, platforms);
+
+  //criação da musica
+  music = this.sound.add("music");
+  music.play({
+    loop: true
+  });
+
+  //fullscreen
+  var button = this.add
+    .image(800 - 16, 16, "fullscreen", 0)
+    .setOrigin(1, 0)
+    .setInteractive();
+
+  var FKey = this.input.keyboard.addKey("F");
+
+  FKey.on(
+    "down",
+    function() {
+      if (this.scale.isFullscreen) {
+        button.setFrame(1);
+        this.scale.stopFullscreen();
+      } else {
+        button.setFrame(0);
+        this.scale.startFullscreen();
+      }
+
+      this.physics.add.collider(player, platforms);
+    },
+    this
+  );
+
+  //  Collide the player and the stars with the platforms
+  this.physics.add.collider(player, platforms);
+
+  var trocacena = this.add
+    .image(500 - 64, 400, "reiniciar", 0)
+    .setOrigin(1, 0)
+    .setInteractive();
+  trocacena.on(
+    "pointerup",
+    function() {
+      music.stop();
+      player.anims.play("turn", false);
+      gameOver = false;
+      this.scene.start(fase1);
+    },
+    this
+  );
+};
+
+gameover1.update = function() {};
+
+export { gameover1 };
